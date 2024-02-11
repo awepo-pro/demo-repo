e0ec98e732c9cdd7b6410ebf63acf4360681d5a2
awepo-pro <dc22762@umac.mo>
2024-02-11 11:43:32 +0800

    remove majority of comments for the sake of simplicity

--- Asp/v2/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ Asp/v2/TodoApi/obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b97144effb6fd309f41919277f6b5edd7b120c55")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b534c55fac45af1d825562a80197a26887e41f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
